d96e3982eacce913a8be0c5d190bd56f0f837d6d
pabmevi <pabmevi@gmail.com>
2025-11-26 11:16:37 -0600

    Adding a new dataset

--- datasets.py
+++ datasets.py
@@ -8,22 +8,28 @@ from ModifiedClass import MyClassifier
 import numpy as np
 import pandas as pd
 from CONFOLD.foldrm import Classifier
-
         
-def extinction_birds15(data_path='datasets/Extinction/BirdTraits_15imputations_mean.csv'):
-    attrs = ["Primary.Lifestyle","RLM","Nest_Type","Nest_SBS","Flightlessness","Family",
-             "Foraging","MatingSystem","NestPlacement",
-             "Territoriality","IslandDwelling","Order1","Diet","Habitat_cat","Migration",
-             "Beak.Length_Culmen","Beak.Length_Nares","Beak.Width","Beak.Depth",
-             "Tarsus.Length","Wing.Length","Kipps.Distance","Secondary1","Hand.Wing.Index",
-             "Tail.Length","Habitat.Density","Min.Latitude","Max.Latitude","RR","ISL","LAT",
-             "Elevational.Range","HB","DB","LogNightLights","LogHumanPopulationDensity",
-             "Range_size","Body_mass","Clutch_size"]
+def final_extinctionrisk(data_path='datasets/Extinction/traits_finalnoNA_26Nov25.csv'):
+    attrs = ["original_name","Residential_IUCN","Agriculture_IUCN","Energy_IUCN","Transportation_IUCN",
+             "Biological_Use_IUCN","Human_Intrusions_IUCN","Natural_Modifications_IUCN","Invasive_IUCN","Pollution_IUCN",
+             "Geological_IUCN","Climate_IUCN","n_threats_IUCN","Habitat.Density_AVONET","Primary.Lifestyle_AVONET",
+             "Beak.Length_Culmen_AVONET","Beak.Length_Nares_AVONET","Beak.Width_AVONET","Beak.Depth_AVONET",
+             "Tarsus.Length_AVONET","Wing.Length_AVONET","Kipps.Distance_AVONET","Secondary1_AVONET","Hand.Wing.Index_AVONET",
+             "Tail.Length_AVONET","Min.Latitude_AVONET","Max.Latitude_AVONET","ISL_BirdBase","RLM_BirdBase","LAT_BirdBase",
+             "NormMin_BirdBase","Elevational.Range_BirdBase","NormMax_BirdBase","HB_BirdBase","DB_BirdBase",
+             "Flightlessness_BirdBase","Adult.survival_GenLength","Age.at.first.breeding_GenLength",
+             "Maximum.longevity_GenLength","GenLength_GenLength","Foraging_BirdBehav","MatingSystem_BirdBehav",
+             "NestPlacement_BirdBehav","Territoriality_BirdBehav","IslandDwelling_BirdBehav","LogNightLights_BirdBehav",
+             "LogHumanPopulationDensity_BirdBehav","Order.IUCN","Family.IUCN","Range_size","Body_mass","IUCN_status2025",
+             "Clutch_size","Diet","Habitat","Migration"
+]
     
-    nums = ["Beak.Length_Culmen","Beak.Length_Nares","Beak.Width","Beak.Depth",
-             "Tarsus.Length","Wing.Length","Kipps.Distance","Secondary1","Hand.Wing.Index",
-             "Tail.Length", "Min.Latitude","Max.Latitude", "Elevational.Range", "HB", "DB",
-             "LogNightLights","LogHumanPopulationDensity", "Range_size", "Body_mass", "Clutch_size"]
+    nums = ["Beak.Length_Culmen_AVONET","Beak.Length_Nares_AVONET","Beak.Width_AVONET","Beak.Depth_AVONET",
+             "Tarsus.Length_AVONET","Wing.Length_AVONET","Kipps.Distance_AVONET","Secondary1_AVONET","Hand.Wing.Index_AVONET",
+             "Tail.Length_AVONET","Min.Latitude_AVONET","Max.Latitude_AVONET","NormMin_BirdBase","Elevational.Range_BirdBase",
+             "NormMax_BirdBase","Adult.survival_GenLength","Age.at.first.breeding_GenLength","Maximum.longevity_GenLength",
+             "GenLength_GenLength", "LogNightLights_BirdBehav","LogHumanPopulationDensity_BirdBehav","Range_size",
+             "Body_mass","Clutch_size"]
     label = "extinction_risk"
     
     model = MyClassifier(attrs=attrs, numeric=nums, label=label)
@@ -87,20 +93,6 @@ def new_extinction_birds_imputed(data_path='datasets/Extinction/BirdTraits_15imp
     model = Classifier(attrs=attrs, numeric=nums, label=label)
     data = model.load_data(data_path) # Use the argument here
     print('\n% extinction birds dataset loaded', np.shape(data))
-    return model, data
-
-def extinction_amphib(data_path='datasets/Extinction/Pottieretal2022_ampthermtoler1.csv'):
-    attrs = ["order","family","latitude","longitude","elevation",
-             "acclimation_temp","acclimation_time",
-             "medium_test_temp","start_temp","ramping","mean_UTL"
-]
-    nums = ["latitude","longitude","elevation",
-             "acclimation_temp","acclimation_time",
-             "medium_test_temp","start_temp","ramping","mean_UTL"]
-
-    model = Classifier(attrs=attrs, numeric=nums, label="IUCN_status")
-    data = model.load_data(data_path)
-    print('\n% dataset', np.shape(data))
     return model, data
 
 
