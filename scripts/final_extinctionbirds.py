4ca2633bba921b1fe579ec019211730ca001ffbf
pabmevi <pabmevi@gmail.com>
2025-12-06 20:31:14 -0600

    d

--- scripts/final_extinctionbirds.py
+++ scripts/final_extinctionbirds.py
@@ -138,6 +138,10 @@ rule5 = "with confidence 0.90 class = 'Higher_risk' if 'Generation_length' '>=' 
 rule6 = "with confidence 0.90 class = 'Lower_risk' if 'Generation_length' '<=' '1.424'"
 rule7 = "with confidence 0.80 class = 'Higher_risk' if 'Body_mass' '>=' '130'"
 rule8 = "with confidence 0.80 class = 'Lower_risk' if 'Body_mass' '<=' '15'"
+rule9 = "with confidence 0.90 class = 'Higher_risk' if 'Agriculture' '=' '1'"
+rule10 = "with confidence 0.90 class = 'Higher_risk' if 'Hunting' '=' '1'"
+rule11 = "with confidence 0.90 class = 'Higher_risk' if 'Invasive_species' '=' '1'"
+rule12 = "with confidence 0.90 class = 'Higher_risk' if 'Climate_change' '=' '1'"
 
 # Add the manual rules to the model
 expert_model.add_manual_rule(rule1, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
@@ -148,6 +152,13 @@ expert_model.add_manual_rule(rule5, model_template.attrs, model_template.numeric
 expert_model.add_manual_rule(rule6, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
 expert_model.add_manual_rule(rule7, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
 expert_model.add_manual_rule(rule8, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule8, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule8, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule8, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule9, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule10, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule11, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+expert_model.add_manual_rule(rule12, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
 
 print("--- Manual Rules Added to the Model (Before Training) ---")
 for rule in expert_model.rules:
@@ -227,6 +238,10 @@ rule5_no_confidence = "class = 'Higher_risk' if 'Generation_length' '>=' '4.068'
 rule6_no_confidence = "class = 'Lower_risk' if 'Generation_length' '<=' '1.424'"
 rule7_no_confidence = "class = 'Higher_risk' if 'Body_mass' '>=' '130'"
 rule8_no_confidence = "class = 'Lower_risk' if 'Body_mass' '<=' '15'"
+rule9_no_confidence = "class = 'Higher_risk' if 'Agriculture' '=' '1'"
+rule10_no_confidence = "class = 'Higher_risk' if 'Hunting' '=' '1'"
+rule11_no_confidence = "class = 'Higher_risk' if 'Invasive_species' '=' '1'"
+rule12_no_confidence = "class = 'Higher_risk' if 'Climate_change' '=' '1'"
 
 # Add the manual rules to the model
 learned_confidence_model.add_manual_rule(rule1_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
@@ -237,6 +252,10 @@ learned_confidence_model.add_manual_rule(rule5_no_confidence, model_template.att
 learned_confidence_model.add_manual_rule(rule6_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
 learned_confidence_model.add_manual_rule(rule7_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
 learned_confidence_model.add_manual_rule(rule8_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+learned_confidence_model.add_manual_rule(rule9_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+learned_confidence_model.add_manual_rule(rule10_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+learned_confidence_model.add_manual_rule(rule11_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
+learned_confidence_model.add_manual_rule(rule12_no_confidence, model_template.attrs, model_template.numeric, ['Lower_risk', 'Higher_risk'], instructions=False)
 
 print("--- Manual Rules Added (Before Training) ---")
 print("Notice the default confidence value of 0.5 assigned to each rule.")
