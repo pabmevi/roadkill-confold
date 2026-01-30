f2de35c5bc6f0ac61b92007b859cbb1619f5d588
pabmevi <pabmevi@gmail.com>
2026-01-30 00:43:52 -0600

    new variables

--- datasets.py
+++ datasets.py
@@ -42,18 +42,19 @@ def final_extinctionrisk_noth(data_path='datasets/Extinction/traits_combined_noN
     return model, data
 
 def rk_mammals(data_path='datasets/Extinction/RkTraits_CONFOLD.csv'):
-    attrs = ["regionContinent","decimalLatitude","decimalLongitude","Order","Family","IUCN","noKM","adult_mass_g",
-             "adult_brain_mass_g","adult_body_length_mm","max_longevity_d","age_first_reproduction_d","gestation_length_d",
-             "litter_size_n","litters_per_year_n","neonate_mass_g","weaning_age_d","generation_length_d","dispersal_km",
-             "density_n_km2","home_range_km2","dphy_invertebrate","det_vend","det_vect","det_vunk","det_scav","det_fruit",
+    attrs = ["decimalLatitude","decimalLongitude","Order","noKM","adult_mass_g",
+             "adult_body_length_mm","max_longevity_d","age_first_reproduction_d","gestation_length_d",
+             "litter_size_n","litters_per_year_n","weaning_age_d","dispersal_km",
+             "density_n_km2","home_range_km2","dphy_invertebrate","det_vend","det_vect","det_scav","det_fruit",
              "det_seed","det_plantother","det_diet_breadth_n","upper_elevation_m","lower_elevation_m","habitat_breadth_n",
-             "roadkill_category"]
-    
+             "road.classe", "relative_brain_size","roadkill_category"]
+
     nums = ["decimalLatitude","decimalLongitude", "noKM","adult_mass_g",
-             "adult_brain_mass_g","adult_body_length_mm","max_longevity_d","age_first_reproduction_d","gestation_length_d",
-             "litter_size_n","litters_per_year_n","neonate_mass_g","weaning_age_d","generation_length_d","dispersal_km",
-             "density_n_km2","home_range_km2","dphy_invertebrate","det_vend","det_vect","det_vunk","det_scav","det_fruit",
-             "det_seed","det_plantother","det_diet_breadth_n","upper_elevation_m","lower_elevation_m","habitat_breadth_n"]
+             "adult_body_length_mm","max_longevity_d","age_first_reproduction_d","gestation_length_d",
+             "litter_size_n","litters_per_year_n","weaning_age_d","dispersal_km",
+             "density_n_km2","home_range_km2","dphy_invertebrate","det_vend","det_vect","det_scav","det_fruit",
+             "det_seed","det_plantother","det_diet_breadth_n","upper_elevation_m","lower_elevation_m","habitat_breadth_n", 
+             "relative_brain_size"]
     label = "roadkill_category"
     
     model = Classifier(attrs=attrs, numeric=nums, label=label)
