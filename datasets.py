5942a3fd805d5cc6237dee01b50f233df5b96244
Anthony Marchiafava <anmarch@okstate.edu>
2026-04-01 12:12:32 -0500

    renamed and adjsuted more files

--- datasets.py
+++ datasets.py
@@ -4,7 +4,22 @@ sys.path.append(os.path.join(os.getcwd(), 'CONFOLD'))
 import numpy as np
 import pandas as pd
 from CONFOLD.foldrm import Classifier
-from ModifiedClass import MyClassifier
+
+class MyClassifier(Classifier):
+        def load_data(self, file, amount=-1):
+            data, self.attrs = use_dataframe(file, self.attrs, self.label, self.numeric, amount)
+            return data
+        
+
+def use_dataframe(file, attrs, label, numeric, amount):
+    df = pd.read_csv(file, sep=',', on_bad_lines='skip') #get the dataframe
+
+    #split into x and y
+    df_x = df[attrs]
+    df_y = df[label]
+    result = pd.concat([df_x,df_y], axis=1)
+    attrs.append(label)
+    return result,attrs
         
 def final_extinctionrisk(data_path='datasets/Extinction/traits_combined_noNA_5Dece25.csv'):
     attrs = ["Order","Family","Agriculture","Hunting","Invasive_species","Climate_change",
